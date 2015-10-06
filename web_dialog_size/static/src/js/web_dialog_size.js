ca126365d138b5f3fe5abd23c0d18d425b669d60
fossoult <thomas.fossoul@noviat.com>
2015-10-06 10:50:30 +0200

    New feature: auto expand selective via context

--- web_dialog_size/static/src/js/web_dialog_size.js
+++ web_dialog_size/static/src/js/web_dialog_size.js
@@ -1,7 +1,6 @@
 openerp.web_dialog_size= function (instance) {
 
-    instance.web.Dialog =  instance.web.Dialog.extend({
-
+    instance.web.Dialog.include({
         init_dialog: function () {
             var self = this;
             this._super();
@@ -14,7 +13,6 @@ openerp.web_dialog_size= function (instance) {
                 self.$dialog_box.find('.dialog_button_restore').on('click', self._restore);
             }
         },
-
         _extending: function() {
             var self = this;
             $(this).parents('.modal-dialog').addClass('dialog_full_screen');
@@ -22,7 +20,6 @@ openerp.web_dialog_size= function (instance) {
 
             $(this).parents('.modal-dialog').find('.dialog_button_restore').removeClass('dialog_button_hide')
         },
-
         _restore: function() {
             var self = this;
             $(this).parents('.modal-dialog').removeClass('dialog_full_screen');
@@ -30,7 +27,30 @@ openerp.web_dialog_size= function (instance) {
 
             $(this).parents('.modal-dialog').find('.dialog_button_extend').removeClass('dialog_button_hide')
         },
+    });
 
+    instance.web.FormView.include({
+        view_loading: function(r) {
+            var extend_view = false;
+            // On a O2M/M2M field context on a form view
+            if (this.ViewManager.context)
+                var context = this.ViewManager.context.eval();
+                if (context && context['web_dialog_extend'])
+                    extend_view = true;
+
+            // On an action context
+            if (this.ViewManager.ActionManager != undefined && this.ViewManager.ActionManager.dialog && this.ViewManager.action.context.web_dialog_extend)
+                extend_view = true;
+            
+            if (extend_view){
+                var b = $('.dialog_button_extend:last');
+                b.parents('.modal-dialog').addClass('dialog_full_screen');
+                b.addClass('dialog_button_hide');
+                b.parents('.modal-dialog').find('.dialog_button_restore').removeClass('dialog_button_hide')
+            }
+            var self = this;
+            return this._super(r);
+        },
     });
 
 };
